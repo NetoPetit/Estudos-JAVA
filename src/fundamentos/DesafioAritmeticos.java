8c5d04016b34f654efd465eb63359c1da646e064
NetoPetit <cidao_petit5@hotmail.com>
2024-10-22 16:39:29 -0300

    atualização

--- src/fundamentos/DesafioAritmeticos.java
+++ src/fundamentos/DesafioAritmeticos.java
@@ -4,12 +4,28 @@ public class DesafioAritmeticos {
 	
 	public static void main(String[] args) {
 		
+		/*
 		int a = 3 * 4 - 10;
 		int b = (int) Math.pow(a, 3); //(CAST)
 		double c = Math.pow(a, 3);
 		
 		System.out.println(b);
 		System.out.println(c);
+		*/
+		int var1 = 6 * (3 + 2);
+		int var2 = (int) Math.pow(var1 , 2) / (3 * 2); //(CAST)
+		
+		int var3 = (1-5) * (2 - 7) / 2;
+		int var4 = (int) Math.pow(var3 , 2);
+		
+		int var5 = var2 - var4;
+		int var6 = (int) Math.pow(var5, 3);
+		
+		int var7 = (int) Math.pow(10, 3);
+		
+		int resultado = var6 / var7;
+		
+		System.out.println(resultado);
 		
 	}
 
